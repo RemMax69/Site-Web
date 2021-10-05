c4a040b5119a93483297d42a3fa90f194d39bdd8
Pernelle <pernelle.pleuvry@cpe.fr>
2021-10-05 12:11:44 +0200

    java fleche

--- scriptimages.js
+++ scriptimages.js
@@ -1,16 +1,46 @@
 var index = 0;
+
 var images = [
     "images/bracelet or.jpg",
-    "images/corde.jpg"
-]
+    "images/corde.jpg",
+    "images/pierrebleue.jpg",
+    "images/pierre violet.jpg",
+    "images/key.jpg",
+    "images/bracelet argent.jpg",
 
-function precedent() {
-    console.log("on a cliqué sur precedent")
-}
+]
 
 function suivant() {
     console.log("on a cliqué sur suivant")
     index = (index + 1) % images.length;
     console.log(images[index])
     document.getElementById("image").src = images[index]
+}
+function precedent() {
+    console.log("on a cliqué sur precedent")
+    if (index == 0) {
+    index=images.length-1
+    }else{
+    index = (index-1)% images.length;}
+    document.getElementById("image").src = images[index]
+}
+
+
+
+function changement(select){
+    console.log(select.value)
+    if (select.value="or") {
+        document.getElementById("image").src = "images/bracelet or.jpg"
+    }
+    if (select.value="argent") {
+        
+        document.getElementById("image").src = "images/bracelet argent.jpg"
+      
+    } 
+    
+    else{
+        document.getElementById("image").src = "images/corde.jpg"
+    }
+    
+
 }
--- scriptperso.js
+++ scriptperso.js
@@ -1,9 +1,9 @@
-console.log("test")
+/* console.log("test")
 let produit = new URLSearchParams(window.location.search).get("produit")
 console.log(produit)
 
  if (produit === "bracelet") {
      document.getElementById("choixcollier").style.display = "none";
  }
-
+ */
 
