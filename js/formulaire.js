1f7a6fbe182cf3418334a1c0edf99a20d3656517
Pernelle Pleuvry <per.pleuvry@gmail.com>
2021-10-26 12:06:30 +0200

    the end

--- js/formulaire.js
+++ js/formulaire.js
@@ -8,4 +8,34 @@ var prixtotal = localStorage.getItem("prix")
 var prix = JSON.parse(prixtotal)
 
 document.getElementById("prix").innerText = prix
-document.getElementById("produitchoisie").src = choisie[choisie.length-1].image
+document.getElementById("produitchoisie").src = choisie[choisie.length - 1].image
+
+function adresse(event) {
+    let addr = encodeURIComponent(event.target.value);
+    console.log(addr);
+    fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+addr+'.json?access_token=AIzaSyCfohSBF8EuXri2Vsh6QSsiJzFYJQYAtbM')
+    then(function (response) {
+        return response.json();
+    })
+    then(function (adress){
+        const objectURL = URL.createObjectURL(adress);
+        myadress.src=objectURL;
+        fetch('"https://api.mapbox.com/directions/v5/mapbox/driving/45.75525843786705, 4.830161540257744?waypoints=0;2&access_token=AIzaSyCfohSBF8EuXri2Vsh6QSsiJzFYJQYAtbM"')
+    });
+}
+
+function coche(checkbox){
+    if(checkbox.checked){
+        prix=prix+8;
+        maj();
+    } else{
+        prix=prix-8;
+        maj();
+    }
+
+}
+
+function maj(){
+    document.getElementById("prix").innerText = prix;
+}
+
